a2039a5eddda265798bde085dfa211bf4c362473
vinay156 <vgoyal@ch.iitr.ac.in>
2021-08-08 20:30:04 +0530

    model name

--- models/posts.js
+++ models/posts.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 let Post = new mongoose.Schema({
   title: String,
   description: String,
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Uomment" }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   date: { type: Date, default: Date.now },
 });
